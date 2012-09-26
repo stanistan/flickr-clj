892ee1c0184190e03548d158ae0c6db755292424
Stan Rozenraukh <stan@stanistan.com>
2012-09-25 21:10:04 -0400

    method info

--- test/flickr_clj/methods_test.clj
+++ test/flickr_clj/methods_test.clj
@@ -2,5 +2,26 @@
   (:use flickr-clj.methods clojure.test))
 
 (deftest method-info-generator
-  (testing "method-info-maker"
-    (is (= 1 0))))
+  (testing "method-info with get"
+    (is (=
+      (method-info :some-method :some.parent)
+      (method-info [:get :some-method] :some.parent)
+      {:request-method :get
+       :key :some-method
+       :parent :some.parent
+       :name "flickr.some.parent.someMethod"})))
+  (testing "method-info with post"
+    (is (=
+      (method-info [:post :some-method] :some.parent)
+      {:request-method :post
+       :key :some-method
+       :parent :some.parent
+       :name "flickr.some.parent.someMethod"})))
+  (testing "method-info with other (put)"
+    (is (=
+      (method-info [:put :some-method] :some.parent)
+      {:request-method :put
+       :key :some-method
+       :parent :some.parent
+       :name "flickr.some.parent.someMethod"}))))
+

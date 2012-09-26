cd810911be04879ba2f050016d3c906a44a7da2a
Stan Rozenraukh <stan@stanistan.com>
2012-09-25 21:31:33 -0400

    more tests

--- test/flickr_clj/methods_test.clj
+++ test/flickr_clj/methods_test.clj
@@ -25,3 +25,42 @@
        :parent :some.parent
        :name "flickr.some.parent.someMethod"}))))
 
+(deftest groups
+  (testing "getting methods in group"
+    (is (=
+      []
+      (get-methods-in-group "does.not.exist")
+      (get-methods-in-group :does.not.exist)))
+    (is (=
+      (get-methods-in-group :favorites)
+      (get-methods-in-group "favorites")
+      [{:request-method :post :key :add :parent :favorites :name "flickr.favorites.add"}
+       {:request-method :get :key :get-context :parent :favorites :name "flickr.favorites.getContext"}
+       {:request-method :get :key :get-list :parent :favorites :name "flickr.favorites.getList"}
+       {:request-method :get :key :get-public-list :parent :favorites :name "flickr.favorites.getPublicList"}
+       {:request-method :post :key :remove :parent :favorites :name "flickr.favorites.remove"}])))
+  (testing "bools"
+    (is (= true
+      (method-group? :favorites)
+      (method-group? :photos)
+      (method-group? "favorites")
+      (method-group? "photos.comments")))
+    (is (= false
+      (method-group? :faaake)
+      (method-group? "some.non.group"))))
+  (testing "getting possible groups"
+    (is (=
+      (get-possible-groups :a.b.c.d)
+      (get-possible-groups "a.b.c.d")
+      [:a.b.c.d :a.b.c :a.b :a])))
+  (testing "get actual group using possible groups"
+    (is (= nil
+      (get-group :a.b.c.d)
+      (get-group :something.that.does.not.exist)))
+    (is (= :favorites
+      (get-group "favorites")
+      (get-group :favorites)))
+    (is (= :photos.comments
+      (get-group "photos.comments")
+      (get-group "photos.comments.a-method")
+      (get-group :photos.comments)))))

6a014aefa0d9d62cc28819b0b1f31a4269ddcef0
Stan Rozenraukh <stan@stanistan.com>
2014-06-29 13:28:57 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.stanistan/flickr-clj "0.1.0"
+(defproject org.clojars.stanistan/flickr-clj "0.1.1"
   :min-lein-version "2.0.0"
   :description "A Clojure wrapper for the flickr API"
   :url "http://github.com/stanistan/flickr-clj"
